4e78cbcd95eb2113c91e8c31bd8b4d5b6fe99976
Yogesh1204 <sainiy744@gmail.com>
2024-03-29 21:47:08 +0530

    html_file

---
+++ index.js
@@ -0,0 +1,13 @@
+const mainHeading = document.getElementById('main-heading');
+mainHeading.textContent = "Fruit World";
+
+const header = document.getElementById('header');
+header.style.color = 'orange';            
+header.style.backgroundColor = 'green';
+header.style.borderBottom = '3px solid orange';
+
+const basketheading = document.getElementById('basket-heading');
+basketheading.style.color = "green";
+
+const thanks = document.getElementById('thanks');
+thanks.innerHTML = "<p>Please visit us again</p>";
